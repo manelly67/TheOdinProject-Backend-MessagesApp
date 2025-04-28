b8593bb9e0072c75d7f24b6ad0b9546fd6f2e990
manellylopez <manellylopezqqq@gmail.com>
2025-04-28 15:01:24 -0500

    modify cors origin

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 /* app.use(cors()); */
 
 app.use(cors({
-    origin: "http://localhost:5173/",
+    origin: "http://localhost:5173",
     allowedHeaders: ["Content-Type", "Connection"],
     credentials:true,
   }));

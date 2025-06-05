fd72de4d3c11d56262e105e0e631330eda7361fc
manellylopez <manellylopezqqq@gmail.com>
2025-06-05 18:23:13 -0500

    add url of the deployed frontend

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 /* app.use(cors()); */
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://whitedove.netlify.app/",
     allowedHeaders:['Content-Type', 'Authorization','Connection'],
     credentials:true,
   }));

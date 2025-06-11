8dd525cb78741055f215ca01b178f74408ba689f
manellylopez <manellylopezqqq@gmail.com>
2025-06-11 11:48:43 -0500

    restore file routes/index.js also deleted

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+const homepage = require("./homepage");
+const signup = require("./signup");
+const login = require("./login");
+const logout = require("./logout");
+const chat = require("./chat");
+const message = require("./message");
+const user = require("./users");
+const login_as_guest = require("./loginasguest");
+const profile = require("./profile");
+
+module.exports = {
+  homepage,
+  signup,
+  login,
+  logout,
+  chat,
+  message,
+  user,
+  login_as_guest,
+  profile,
+};
